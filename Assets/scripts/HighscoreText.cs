884ed70ecfc0bb05abd39cee6cf400b6f0163ead
PhucNguyen96 <nguyenphuc.ddt@gmail.com>
2021-01-13 16:15:23 +0700

    First commit

---
+++ Assets/scripts/HighscoreText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class HighscoreText : MonoBehaviour 
+{
+
+	Text score;
+
+	// use void OnEnable instead Start
+	// this text needs to be refreshed everytime the start page is activated
+	void OnEnable() 
+	{
+		score = GetComponent<Text>();
+		score.text = "High Score: " + PlayerPrefs.GetInt("HighScore").ToString();
+	}
+}
---
+++ Assets/scripts/ScoreText.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ScoreText : MonoBehaviour
+{
+	Text score;
+
+	// use void OnEnable instead Start
+	// this text needs to be refreshed everytime the start page is activated
+	void OnEnable()
+	{
+		score = GetComponent<Text>();
+		score.text = "Score: " + GameManager.Instance.Score;
+	}
+}
